c0c9c3c610d49320580951676387a91c3145520b
Max Bulygin <maksym.bulygin@gmail.com>
2016-07-18 15:30:34 +0300

    changes after mentor's review

--- js/index.js
+++ js/index.js
@@ -1,60 +1,75 @@
-var progTest = {
-    header: "Тест по программированию",
-    question: "Вопрос №",
-    answer: " Вариант ответа №",
-    button: "Проверить мои результаты",
-    getDiv: function (tagName, clName) {
-        var tag = document.createElement(tagName);
-        tag.classList.add(clName);
-        document.body.appendChild(tag);
-    },
-    getInnerTag: function (tagName, clName, parent) {
-        var tag = document.createElement(tagName);
-        tag.classList.add(clName);
-        document.querySelector(parent).appendChild(tag);
-    },
-    getHeader: function (hNum, selector) {
-        var tag = document.createElement(hNum);
-        tag.innerHTML = this.header;
-        document.querySelector(selector).appendChild(tag);
-    },
-    getListItems: function (queAmount) {
-        for (i = 0; i < queAmount; i++){
-            this.getInnerTag('li', 'list-item', '.list');
-        }
-    },
-    getQuestions: function () {
-        var listItems = document.querySelectorAll('.list-item');
-        for (i=0; i<listItems.length; i++) {
-            listItems[i].innerHTML = "<h3>" + this.question + (i+1) + "</h3>";
-        }
-    },
-    getAnswers: function () {
-        var listItems = document.querySelectorAll('.list-item');
-        var i = 0;
-        while (i<listItems.length) {
-            for (j=0; j<3; j++) {
-                var tag = document.createElement('p');
-                listItems[i].appendChild(tag);
-                tag.innerHTML = "<label><input type=\"checkbox\"> " + this.answer + (j+1) + "</label>";
-            }
-            i++;
-        }
-    },
-    getButton: function () {
-        this.getInnerTag('button', 'btn', '.col-md-12');
-        var but = document.querySelector('.btn');
-        but.innerHTML = this.button;
-    }
-}
+var progTest = (function() {
+    var createTest = {
+        header: "Тест по программированию",
+        question: "Вопрос №",
+        answer: " Вариант ответа №",
+        button: "Проверить мои результаты",
+        getDiv: function (tagName, clName) {
+            var tag = document.createElement(tagName);
 
-progTest.getDiv('div', 'container');
-progTest.getInnerTag('div', 'row', '.container');
-progTest.getInnerTag('div', 'col-md-12', '.row');
-progTest.getHeader('h1', '.col-md-12');
-progTest.getInnerTag('ol', 'list', '.col-md-12');
-progTest.getListItems(3);
-progTest.getQuestions();
-progTest.getAnswers();
-progTest.getButton();
+            tag.classList.add(clName);
+            document.body.appendChild(tag);
+        },
+        getInnerTag: function (tagName, clName, parent) {
+            var tag = document.createElement(tagName);
+
+            tag.classList.add(clName);
+            document.querySelector(parent).appendChild(tag);
+        },
+        getHeader: function (hNum, selector) {
+            var tag = document.createElement(hNum);
+
+            tag.innerHTML = this.header;
+            document.querySelector(selector).appendChild(tag);
+        },
+        getListItems: function (queAmount) {
+            for (var i = 0; i < queAmount; i++) {
+                this.getInnerTag('li', 'list-item', '.list');
+            }
+        },
+        getQuestions: function () {
+            var listItems = document.querySelectorAll('.list-item');
+
+            for (var i = 0; i < listItems.length; i++) {
+                listItems[i].innerHTML = "<h3>" + this.question + (i + 1) + "</h3>";
+            }
+        },
+        getAnswers: function () {
+            var listItems = document.querySelectorAll('.list-item');
+
+            for (var i = 0; i < listItems.length; i++) {
+                for (var j = 0; j < 3; j++) {
+                    var tag = document.createElement('p');
+
+                    listItems[i].appendChild(tag);
+                    tag.innerHTML = '<label><input type="checkbox"> ' + this.answer + (j + 1) + '</label>';
+                }
+            }
+        },
+        getButton: function () {
+            this.getInnerTag('button', 'btn', '.col-md-12');
+            var but = document.querySelector('.btn');
+
+            but.innerHTML = this.button;
+        },
+        init: function () {
+            this.getDiv('div', 'container');
+            this.getInnerTag('div', 'row', '.container');
+            this.getInnerTag('div', 'col-md-12', '.row');
+            this.getHeader('h1', '.col-md-12');
+            this.getInnerTag('ol', 'list', '.col-md-12');
+            this.getListItems(3);
+            this.getQuestions();
+            this.getAnswers();
+            this.getButton();
+        }
+    };
+
+    createTest.init();
+
+    return createTest;
+})();
+
+
+
 
--- js/testing.js
+++
@@ -1,3 +0,0 @@
-a = new Array (33, 2, 44, 21, 123, 67, 56);
-a.sort(function(a,b){return a-b});
-console.log(a);
